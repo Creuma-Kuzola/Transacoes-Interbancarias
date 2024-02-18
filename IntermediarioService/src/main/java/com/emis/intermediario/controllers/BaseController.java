f6b6f0c53a7a3fee14099997f979f23ab475c631
Creuma-Kuzola <creumakuzola@gmail.com>
2024-02-18 15:11:28 +0100

    feat: adding Transferencia Service in Kuzola Bank Project

--- IntermediarioService/src/main/java/com/emis/intermediario/controllers/BaseController.java
+++
@@ -1,7 +0,0 @@
-package com.emis.intermediario.controllers;
-
-import com.emis.intermediario.https.utils.ResponseControllerUtils;
-
-public class BaseController extends ResponseControllerUtils {
-
-}
