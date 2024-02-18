156c1bf3f3f219a4e3291af45c4ed842af03f1f3
Creuma-Kuzola <creumakuzola@gmail.com>
2024-02-18 16:17:50 +0100

    feat: TransferenciaService backup done

--- TransferenciaService/src/main/java/com/example/TransferenciaService/entities/Transferencia.java
+++ TransferenciaService/src/main/java/com/example/TransferenciaService/entities/Transferencia.java
@@ -7,19 +7,8 @@ package com.example.TransferenciaService.entities;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import jakarta.persistence.*;
+
 
 /**
  *
--- TransferenciaService/src/main/java/com/example/TransferenciaService/entities/User.java
+++ TransferenciaService/src/main/java/com/example/TransferenciaService/entities/User.java
@@ -5,17 +5,8 @@
 package com.example.TransferenciaService.entities;
 
 import java.io.Serializable;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
+import jakarta.persistence.*;
+
 
 /**
  *
